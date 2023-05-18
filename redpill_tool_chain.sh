6984cf4ff054b66b01ab00316de5f4670877cb59
nb12nb34 <38234244+nb12nb34@users.noreply.github.com>
2023-05-19 06:34:51 +0800

    Update redpill_tool_chain.sh

--- redpill_tool_chain.sh
+++ redpill_tool_chain.sh
@@ -324,14 +324,10 @@ if [[ "${ACTION}" != "del" && "${ACTION}" != "add" && "${ACTION}" != "sn" && "${
         # TODO: fix: wait new toolkit
         if [ "${DSM_VERSION}" == "7.1" ]; then
             DSM_VERSION="7.0"
+        else
+            DSM_VERSION="7.2"
         fi
         EXTRACTED_KSRC="/usr/local/x86_64-pc-linux-gnu/x86_64-pc-linux-gnu/sys-root/usr/lib/modules/DSM-${DSM_VERSION}/build/"
-         
-        if [ "${DSM_VERSION}" == "7.2" ]; then
-           DSM_VERSION="7.2"
-        fi
-        EXTRACTED_KSRC="/usr/local/x86_64-pc-linux-gnu/x86_64-pc-linux-gnu/sys-root/usr/lib/modules/DSM-${DSM_VERSION}/build/"
-        
     fi
 else
     if [[ "${ACTION}" != "del" && "${ACTION}" != "add" && "${ACTION}" != "sn" && "${ACTION}" != "clean" ]]; then
